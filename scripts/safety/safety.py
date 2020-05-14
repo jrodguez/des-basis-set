4d812572b1902003d0e6c6b033df46ef4464b05c
jrodguez <jrodguez@uw.edu>
2020-05-13 18:30:21 -0700

    Included boolean statements for multiple cases, converted code into functions and main wrapper that will work on a large set of chemcial cid's

--- scripts/safety/safety.py
+++ scripts/safety/safety.py
@@ -3,38 +3,89 @@ import requests
 import json
 import pubchempy as pcp
 
-#Using the url below, we can access the GHS classifictaion data for a compound on pubchem
-#testing with retrieving ghs classifications for choline chloride (cid = 6209)
-safety_url = "https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/6209/JSON?heading=GHS+Classification"
 
-request = requests.get(safety_url)
-request_json = request.json()
+def check_GHS_data(request_json):
+    """This function checks to see if GHS safety information data is available
+    in the pubchem data file for a chemical"""
 
-#The data is basially a giant dictionary and there is a specific block that contains the GHS information we want. 
-# Parsing through the dictionary to get to the desired block of text, each sentance is looped through and added to a list
-GHS_information_list = []  # this list will contain the block of GHS information
-for i in range(len(request_json['Record']['Section'][0]['Section'][0]['Section'][0]['Information'][2]['Value']['StringWithMarkup'])):
-    temp_list = []  # temporary list each sentance gets added to before appending to GHS list
-    temp_list.append(request_json['Record']['Section'][0]['Section'][0]['Section']
-                     [0]['Information'][2]['Value']['StringWithMarkup'][i]['String'])
-    GHS_information_list.append(temp_list)
+    if 'Fault' in request_json:  # first key in dict will be Fault if no GHS heading in json data
+        return 'No GHS data available'
 
-#While this is all important information, we want to specifically extract the statements that contain the GHS hazard codes
-# list that will contain the hazrad codes and their descriptions.
-hazard_description_list = []
-for item in GHS_information_list:
-    # list comprehension, keeps lists that start with H, i.e. the hazard code
-    temp_haz = [idx for idx in item if idx[0] == 'H']
-    hazard_description_list.append(temp_haz)
-    #There will be empty lists so this step removes them
-    for item in hazard_description_list:
-        if len(item) == 0:
-            hazard_description_list.remove(item)
+    else:
+        return 'GHS data available'
 
-#Now that we have the hazard code and description, 
-# we can go in and extract the hazard code to a list by splitting the string and retrieving the 1st substring, which is the hazard code
-hazard_code_list = []
-for item in hazard_description_list:
-    string = item[0]
-    hazard = string.split(' ', 1)[0]
-    hazard_code_list.append(hazard)
+
+def hazard_classification(request_json):
+    """This function checks if the subsatnce is classified as hazardous or non hazardous if GHS data was found"""
+
+    GHS_status = check_GHS_data(request_json)
+
+    # cas if no data was found in ghs retrieval function
+    if GHS_status == 'No GHS data available':
+        return GHS_status
+
+    #otherwise, continue to parse through the json file to determine if the substance is hazardous or not
+    else:
+        if len(request_json['Record']['Section'][0]['Section'][0]['Section'][0]['Information']) == 1:
+            return 'Not classified as a hazardous substance'
+
+        else:
+            return 'Hazardous substance'
+
+
+def get_hazard_codes(cid):
+
+    """This is the main wrapper function for retrieving GHS hazard codes"""
+
+    safety_url = "https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/%s/JSON?heading=GHS+Classification" % str(cid)
+
+    request = requests.get(safety_url)
+    request_json = request.json()
+
+
+    hazard_status = hazard_classification(request_json)
+
+    if hazard_status == 'No GHS data available':
+        return hazard_status
+
+    elif hazard_status == 'Not classified as a hazardous substance':
+        return hazard_status
+
+    elif hazard_status == 'Hazardous substance':
+        
+        GHS_information_list = [] #list that contains GHS information in which hazard codes are located.
+        
+        for i in range(len(request_json['Record']['Section'][0]['Section'][0]['Section'][0]['Information'][2]['Value']['StringWithMarkup'])):
+            temp_list = [] #temporary list each sentance gets added to before appending to GHS list
+            temp_list.append(request_json['Record']['Section'][0]['Section'][0]['Section'][0]['Information'][2]['Value']['StringWithMarkup'][i]['String'])
+            GHS_information_list.append(temp_list)
+            
+        #this portion checks for lists with empty string '' that will break the code if not removed
+        for item in GHS_information_list:
+            if '' not in item: 
+                pass
+        
+            elif '' in item:
+                index = GHS_information_list.index(item)
+                GHS_information_list[index].remove('')
+        
+            
+        hazard_description_list = [] #list that will contain the hazard codes and their descriptions. 
+        
+        for item in GHS_information_list:
+            temp_haz = [idx for idx in item if idx[0] == 'H'] #list comprehension, keeps lists that start with H, i.e. the hazard code
+            hazard_description_list.append(temp_haz)
+            #There will be empty lists so this step removes them
+            for item in hazard_description_list:
+                if len(item) == 0:
+                    hazard_description_list.remove(item)
+                    
+        hazard_code_list = [] #list that contains all of the hazard codesfor the chemcial
+        
+        for item in hazard_description_list:
+            string = item[0]
+            hazard = string.split(' ', 1)[0]
+            hazard_code_list.append(hazard)
+            
+        return hazard_code_list
+                    
